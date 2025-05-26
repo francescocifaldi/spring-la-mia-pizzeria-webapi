058445b0fb811c437d56e8747db41bb7dc361e66
francescocifaldi <francesco.cifaldi@gmail.com>
2025-05-26 21:57:00 +0200

    ADD create, update and delete for pizzarestcontroller

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_webapi/controller/PizzaRestController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_webapi/controller/PizzaRestController.java
@@ -7,8 +7,12 @@ import org.lessons.java.spring_la_mia_pizzeria_webapi.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/api/pizzas")
@@ -27,6 +31,25 @@ public class PizzaRestController {
     public Pizza show(@PathVariable Integer id) {
         Pizza pizza = pizzaService.getById(id);
         return pizza;
+    }
+
+    @PostMapping("/create")
+    public Pizza create(@RequestBody Pizza pizza) {
+        Pizza newPizza = pizzaService.create(pizza);
+        return newPizza;
+    }
+
+    @PutMapping("/{id}")
+    public Pizza update(@PathVariable Integer id, @RequestBody Pizza pizza) {
+        pizza.setId(id);
+        Pizza updatedPizza = pizzaService.update(pizza);
+
+        return updatedPizza;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        pizzaService.deleteById(id);
     }
 
 }
